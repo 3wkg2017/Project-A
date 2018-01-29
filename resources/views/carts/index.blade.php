a24f34e2d90aa50b264658c55d0ff23581297ac3
Konstantin <3wkg2017@gmail.com>
2018-01-29 21:27:47 +0200

    Show Cart small fix

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -4,30 +4,32 @@
 	<div class="row">
 			
 			<form method="POST" action=""> 
-			
+				<div class="col-xs-12 col-md-4">
 					@foreach($carts as $cart)
 					<?php
 						$dish = $cart->dishes;
 						//dd($dish);
 					?>
 
-					<div class="col-xs-12 col-md-4">
+				
 					<div class="card" style="width: 300px;">
 					  <img class="card-img-top" style="max-width: 100%" src="{{$dish->dish_picture}}" alt="">
 					  <div class="card-block">
 					    <h4 class="card-title">{{$dish->dish_name}}</h4>
 					    <p class="card-text">{{$dish->dish_description}}</p>
 					  </div>
-					  <div class="card-block">
-					      <p class="card-text">{{$dish->dish_price}}</p>
-					  	  <button class="btn btn-success">Check out</button>
-					  	  <br>
-					  </div>
+					 
 					</div>
-					</div>
+				
 					@endforeach
-		
-		</form>
+					 <div class="card-block">
+					  	<p class="card-text">{{$dish->dish_price}}</p>
+					  	<button class="btn btn-success">Check out</button>
+					  	<br>
+					 </div>
+					
+				</div>
+			</form>
 	</div>
 </main>
 @endsection
